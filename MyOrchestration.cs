ea65c333a3c3d628239676d4a1e9bd6e328a4915
Anatoli Beliaev <anatolib@microsoft.com>
2025-04-14 14:43:57 -0700

    Delay in activity

--- MyOrchestration.cs
+++ MyOrchestration.cs
@@ -36,10 +36,15 @@ namespace Company.Function
         }
 
         [Function(nameof(SayHello))]
-        public static string SayHello([ActivityTrigger] string name, FunctionContext executionContext)
+        public static async Task<string> SayHello([ActivityTrigger] string name, FunctionContext executionContext)
         {
             ILogger logger = executionContext.GetLogger("SayHello");
             logger.LogInformation("Saying hello to {name}.", name);
+            
+            logger.LogInformation("Delaying...");
+            await Task.Delay(TimeSpan.FromSeconds(60));
+            logger.LogInformation("Delay complete for {name}.", name);
+            
             return $"Hello {name}!";
         }
 
